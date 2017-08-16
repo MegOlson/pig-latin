ab4968cde011e35cc03d86fefef61ff30cda0673
Kristen Kulha and Megan Olson <megan@>
2017-08-16 09:56:20 -0700

    Create variable for user input and check to see if input is a number.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var pigLatin = function(userWord) {
+  var number = parseInt(userWord);
+  if (number === number) {
+    return userWord;
+  } else {
+
+  }
+}
+
+
+$(document).ready(function(){
+  $("#form").submit(function(event){
+    event.preventDefault();
+    var userWord = $("input#word").val();
+    var result = pigLatin(userWord);
+    $("#result").text(result);
+  });
+});
